86fb5a382ccde676d433fb30ee34f0836ba195e4
Supay3 <elze715@gmail.com>
2021-02-12 22:54:00 +0100

    style: started the front of the homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\Product\ProductCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: RouteName::HOME)]
-    public function index(): Response
+    public function index(ProductCategoryRepository $productCategoryRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'productCategories' => $productCategoryRepository->findAll(),
+        ]);
     }
 }
